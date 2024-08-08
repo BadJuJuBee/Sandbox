03608b315fc7168f41027b283fd960d27f9c1fd7
Alexis Vandekieft <lexi.o.vandekieft@gmail.com>
2024-08-07 19:46:28 -0500

    Completed controlflow assignment

--- ControlFlow/ControlFlow.App/Program.cs
+++ ControlFlow/ControlFlow.App/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ControlFlow.App;
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace ControlFlow.App;
 
 public class Program
 {
@@ -18,18 +21,66 @@ public class Program
     // Method to check if a number is even
     public static bool IsDog(string animal)
     {
-        throw new NotImplementedException();
+        if (animal.ToLower() == "dog")
+        {
+            return true;
+        }
+        return false;
     }
 
     // Method to get the animal sound for a dog, cat, cow, or sheep
     public static string GetAnimalSound(string animal)
+    {   switch (animal.ToLower())
     {
-        throw new NotImplementedException();
+        case "dog":
+        {
+            return "bark";
+        }
+        case "cat":
+        {
+            return "meow";
+        }
+        case "cow":
+        {
+            return "moo";
+        }
+        case "sheep":
+        {
+            return "baa";
+        }
+        default:
+        {
+            return "unknown sound";
+        }
+    }
+        // if (animal.ToLower() == "dog")
+        // {
+        //     return "bark";
+        // }
+        // else if (animal.ToLower() == "cat")
+        // {
+        //     return "meow";
+        // }
+        // else if (animal.ToLower() == "cow")
+        // {
+        //     return "moo";
+        // }
+        // else if (animal.ToLower() == "sheep")
+        // {
+        //     return "baa";
+        // }
+        // return "unknown sound";
     }
 
     // Method to print the animal sound 10 times
     public static void PrintAnimalSound10Times(string sound)
     {
-        throw new NotImplementedException();
+        int index = 0;
+        do
+        {
+            Console.WriteLine(sound);
+            index = index + 1;
+        }  while(index < 10);
     }
+
 }
